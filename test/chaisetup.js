46e49211671a471c2e694874af46e533950b0a7c
Krittawat Wisedsuwon <krittawat.wis@lightnet.io>
2021-04-07 16:32:41 +0700

    improve chaisetup

--- test/chaisetup.js
+++ test/chaisetup.js
@@ -1,14 +1,17 @@
 "use strict";
 var chai = require("chai");
-// chai.use(require("chai-events"));
 
 const BN = web3.utils.BN;
+
 const chaiBN = require('chai-bn')(BN);
 chai.use(chaiBN);
 
 var event = require("chai-events");
-var chaiAsPromised = require("chai-as-promised");
-const should = chai.should();
 chai.use(event);
+
+var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
+
+const should = chai.should();
+
 module.exports = chai;
